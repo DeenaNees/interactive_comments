8b39759a8aeb394fd9495765fbf9dcc043b1e69d
abdurraa <abdurrahman.abuhijleh@amdocs.com>
2022-08-07 14:28:46 +0300

    remove node_modules

--- src/components/User.js
+++ src/components/User.js
@@ -3,9 +3,10 @@ import imge from "../images/avatars/image-amyrobson.png";
 function User(props) {
   // image, user name
   // const image = require("" +props.user.image.png);
+
   return (
     <div className="user__container">
-      {/* <img src={image} alt="" /> */}
+      <img src={props.image} alt="" />
       <div>{props.user.username}</div>
     </div>
   );
